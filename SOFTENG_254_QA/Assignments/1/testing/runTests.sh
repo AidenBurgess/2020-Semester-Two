47ebfb0eb41b7c01e4169236ac173f6bb2c664df
Aiden Burgess <aiden9874@gmail.com>
2020-08-18 16:52:22 +1200

    Update testing scripts

--- SOFTENG_254_QA/Assignments/1/testing/runTests.sh
+++ SOFTENG_254_QA/Assignments/1/testing/runTests.sh
@@ -3,23 +3,23 @@ GREEN='\033[1;32m' # define green for SUCCess
 NC='\033[0m'
 
 # Compile
-javac -cp "junit-4.12.jar;hamcrest-core-1.3.jar;." text/TestFlushLeft.java
+javac -cp "junit-4.13.jar;hamcrest-core-1.3.jar;." text/TestFlushLeft.java
 
 errorsFound=0
-for i in good.jar badA.jar badB.jar badC.jar badD.jar badE.jar badF.jar badG.jar badH.jar badI.jar badJ.jar; do
-    printf "$i: "
-    if [ $1 ]; then
-        quiet=`java -cp "junit-4.12.jar;hamcrest-core-1.3.jar;$i;." org.junit.runner.JUnitCore text.TestFlushLeft | grep "Tests run"`
+for i in good.jar badA.jar badB.jar badC.jar badD.jar badE.jar badF.jar badG.jar badH.jar badI.jar badJ.jar badK.jar badL.jar badM.jar badN.jar badO.jar; do
+    printf "%s: " "$i"
+    if [ "$1" ]; then
+        quiet=$(java -cp "junit-4.13.jar;hamcrest-core-1.3.jar;$i;." org.junit.runner.JUnitCore text.TestFlushLeft | grep "Tests run")
     else
-        java -cp "junit-4.12.jar;hamcrest-core-1.3.jar;$i;." org.junit.runner.JUnitCore text.TestFlushLeft
+        java -cp "junit-4.13.jar;hamcrest-core-1.3.jar;$i;." org.junit.runner.JUnitCore text.TestFlushLeft
     fi
     if [ -z "$quiet" ]; then
         printf "No Failures\n"
     else
         printf "${GREEN}FAILED: $quiet\n${NC}"
-        let "errorsFound++"
+        (( errorsFound++ ))
     fi
 done
-if [ $1 ]; then
-    printf "Found errors in $errorsFound / 10 JARs\n"
+if [ "$1" ]; then
+    printf "Found errors in %s / 15 JARs\n" "$errorsFound"
 fi
---
+++ SOFTENG_254_QA/Assignments/1/testing/text/moveTestAndRun.sh
@@ -0,0 +1,1 @@
+rm ../../text/TestFlushLeft.java ; cp TestFlushLeft.java ../../text/TestFlushLeft.java ; cd ../../ ; ./runTests.sh quiet ; cd - ; cd ..
